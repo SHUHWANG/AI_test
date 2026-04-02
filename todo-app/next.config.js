5d807d127f404e94520c8b032444b01b880afc2d
wanghao <wh139136348@126.com>
2026-04-02 16:28:56 +0800

    配置 serverActions 允许自定义域名

--- todo-app/next.config.js
+++ todo-app/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverActions: {
+     allowedOrigins: [
+        'aitest1.20010126.xyz',
+        'localhost:3000',        // 本地开发
+        '*.edgeone.app',         // 允许 EdgeOne 默认域名
+    ],
+  },
+};
 
 module.exports = nextConfig;
